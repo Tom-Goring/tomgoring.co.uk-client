bc17fe6d7ac9736a48ba01fe17011e6cfce9b645
Tom Goring <tgoring97@gmail.com>
2020-06-18 23:35:06 +0100

    Add ball interface and random helpers

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,9 +1,40 @@
 import React, { useState, useLayoutEffect } from "react";
 
+const ball_color = {
+    r: 102,
+    g: 178,
+    b: 255,
+  },
+  R = 2,
+  alpha_f = 0.03,
+  link_line_width = 0.8,
+  dis_limit = 260,
+  directions = ["top", "right", "bottom", "left"];
+
+interface Ball {
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  r: number;
+  alpha: number;
+  phase: number;
+  type: string;
+}
+
+function randomArrayItem(arr: number[]): number {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+function randomNumFrom(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
 export default () => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
   const targetRef = React.useRef<HTMLDivElement>(null);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  const [balls, setBalls] = useState<Ball[]>([]);
 
   useLayoutEffect(() => {
     const updateSize = () => {
@@ -20,7 +51,7 @@ export default () => {
   }, []);
 
   return (
-    <div ref={targetRef} style={{ width: "100%", height: "100%" }}>
+    <div ref={targetRef} style={{ width: "100%", height: "100%", backgroundColor: "#252934" }}>
       <canvas width={dimensions.width} height={dimensions.height} ref={canvasRef}></canvas>
     </div>
   );
